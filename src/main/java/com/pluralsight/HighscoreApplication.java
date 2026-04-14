5fc9dbeda02f616cca0535b93896aeaa345f03f1
Dollant <ariawilliams2425@gmail.com>
2026-04-14 10:13:47 -0400

    Added while loop to reprompt user on invalid input instead of exiting and added defensive coding to validate input format and segment counts.

--- src/main/java/com/pluralsight/HighscoreApplication.java
+++ src/main/java/com/pluralsight/HighscoreApplication.java
@@ -5,7 +5,15 @@ import java.util.Scanner;
 public class HighscoreApplication {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+
+    while (true) {
         System.out.print("Please enter game score ^.^: ");
         String input =  keyboard.nextLine().trim();
+
+        if (!input.contains("|")) {
+            System.out.println("Invalid format! T^T\n Please use: Home:Visitor|score1:score2");
+            continue;
+        }
+}
     }
 }
